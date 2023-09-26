dc11071b956533a53a8e1b295a5c5206dfee2b1e
yeivio <loganjavi@gmail.com>
2023-09-26 18:39:24 +0200

    Added new input system
    Changed the player controller code to use the new system instead of the old one

--- Assets/Scripts/GameScripts/Player/PlayerController.cs
+++ Assets/Scripts/GameScripts/Player/PlayerController.cs
@@ -1,36 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
 
-    public float speed = 7f;
+    public float currentSpeed = 7f; //currentSpeed
+    public float sprintSpeed = 12f; //Sprint player speed
+    public float walkSpeed = 7f; //Walk player speed
     public float turnSmoothTime= 0.1f;
-    public float turnSmoothVelocity;
+    public float turnSmoothVelocity= 0f;
 
     
+    private PlayerInput playerInput;
+
+    private void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        /*      OLD SYSTEM      */
 
-        if(direction.magnitude >= 0.1f)
+        //float horizontal = Input.GetAxisRaw("Horizontal");
+        //float vertical = Input.GetAxisRaw("Vertical");
+        //Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+
+        Vector2 movementInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        Vector3 direction = new Vector3(movementInput[0], 0, movementInput[1]);
+
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            Debug.DrawLine(gameObject.transform.position,direction);
-            controller.Move(direction * speed * Time.deltaTime);
+            controller.Move(direction * currentSpeed * Time.deltaTime);
         }
 
     }
 
+    public void Sprint(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            this.currentSpeed = sprintSpeed;
+        else
+            this.currentSpeed = walkSpeed;
+
+
+    }
     
 }
