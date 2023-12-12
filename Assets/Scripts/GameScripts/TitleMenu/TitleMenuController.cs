5dcad2172c6d475fd625553233e5e2de0742d049
yeivio <loganjavi@gmail.com>
2023-12-12 21:57:23 +0100

    Added menu text animation
    Now on the menu appears a click anything to start the game

--- Assets/Scripts/GameScripts/TitleMenu/TitleMenuController.cs
+++ Assets/Scripts/GameScripts/TitleMenu/TitleMenuController.cs
@@ -1,16 +1,20 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class TitleMenuController : MonoBehaviour
 {
     [SerializeField] private GameObject panelLoading;
+    [SerializeField] private AudioSource theme_song;
+
 
     void Update()
     {
         if (Input.anyKey) {
             panelLoading.GetComponent<Animator>().enabled = true;
-
+            StartCoroutine(Countdown());
         }
     }
 
@@ -18,4 +22,22 @@ public class TitleMenuController : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
+    IEnumerator Countdown()
+    {
+        float timeElapsed = 0;
+        float objetiveTime = 1.5f;    // Time to turn down the volume
+        float initialVolume = this.theme_song.volume;
+
+        while (timeElapsed < objetiveTime)
+        {
+            this.theme_song.volume = Mathf.Lerp(initialVolume, 0f, timeElapsed/objetiveTime);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        this.theme_song.Stop();
+        this.theme_song.volume = initialVolume;
+    }
+
 }
