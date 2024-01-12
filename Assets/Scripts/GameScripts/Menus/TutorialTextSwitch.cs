5d0609430d5d22fb11896f5afd2fbe91bbb0c74d
yeivio <loganjavi@gmail.com>
2024-01-12 22:41:09 +0100

    Added new tutorial menu variations

--- Assets/Scripts/GameScripts/Menus/TutorialTextSwitch.cs
+++ Assets/Scripts/GameScripts/Menus/TutorialTextSwitch.cs
@@ -10,6 +10,12 @@ public class TutorialTextSwitch : MonoBehaviour
     [SerializeField] private GameObject free_move;
     [SerializeField] private GameObject UI;
     [SerializeField] private GameObject NPC_Talk;
+    [SerializeField] private GameObject NPC_Sell;
+    [SerializeField] private GameObject NPC_Buy;
+
+
+    [SerializeField] private TalkableObject NPC_Seller_OBJ; // Npc player object
+
     void Start()
     {
         playerInput = FindAnyObjectByType<PlayerInput>();
@@ -19,6 +25,15 @@ public class TutorialTextSwitch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(NPC_Seller_OBJ.isActive && NPC_Seller_OBJ.tiendaAbierta)
+        {
+            if(NPC_Seller_OBJ.isBuyShop)
+                activarUI(NPC_Buy);
+            else
+                activarUI(NPC_Sell);
+            return;
+        }
+
         if (playerInput.currentActionMap == playerInput.actions.FindActionMap(Utils.FREEMOVE_INPUTMAP) && !this.free_move.activeSelf)
             activarUI(free_move);
         if (playerInput.currentActionMap == playerInput.actions.FindActionMap(Utils.UI_INPUTMAP) && !this.UI.activeSelf)
@@ -32,6 +47,11 @@ public class TutorialTextSwitch : MonoBehaviour
         free_move.SetActive(false);
         UI.SetActive(false);
         NPC_Talk.SetActive(false);
+        NPC_Sell.SetActive(false);
+        NPC_Buy.SetActive(false);
         obj.SetActive(true);
     }
+
+
+
 }
