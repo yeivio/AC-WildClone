7606ad4b1f5f26535ee7d1ad76e473f2902db283
David Florez Mazuera <pandafm200@gmail.com>
2024-01-14 12:05:28 +0100

    Minor fix sell menu

--- Assets/Scripts/GameScripts/Menus/ShopMenu/ToLeftMenu.cs
+++ Assets/Scripts/GameScripts/Menus/ShopMenu/ToLeftMenu.cs
@@ -6,12 +6,4 @@ using UnityEngine.UI;
 public class ToLeftMenu : MonoBehaviour
 {
     [SerializeField] private SellMenu tienda;
-    void Update()
-    {
-        if (tienda.selected == this.GetComponent<Button>() && Input.GetKeyDown(KeyCode.Space))
-        {
-            tienda.NextPage();
-
-        }
-    }
 }
